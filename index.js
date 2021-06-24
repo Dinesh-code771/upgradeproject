4864423e275236d52942f4fcc05ea8df43901c11
Dinesh-code771 <76174638+Dinesh-code771@users.noreply.github.com>
2021-06-24 22:59:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,68 @@
 
-document.getElementById("button1").addEventListener("click", function(){
+function popup(name,box,cross){
+document.getElementById(name).addEventListener("click", function(){
    
-    document.getElementById("sbox").style.display="block";
+    document.getElementById(box).style.display="block";
 })
 
-document.querySelector("span").onclick = function() {
-    console.log("adc")
-    document.getElementById("sbox").style.display = "none";
+document.querySelector("."+cross).onclick = function() {
+    document.getElementById(box).style.display = "none";
   }
   
   // When the user clicks anywhere outside of the modal, close it
   window.onclick = function(event) {
     if (event.target == sbox) {
-        document.getElementById("sbox").style.display = "none";
+        document.getElementById(box).style.display = "none";
     }
 }
+}
+function login(){
+    document.getElementById("button2").addEventListener("click", function(){
+   
+        document.getElementById("sbox2").style.display="block";
+    })
+    
+    document.querySelector(".close2").onclick = function() {
+        document.getElementById("sbox2").style.display = "none";
+      }
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == sbox2) {
+            document.getElementById("sbox2").style.display = "none";
+        }
+    }
+
+}
+function addpost(){
+    document.getElementById("button3").addEventListener("click", function(){
+   
+        document.getElementById("sbox3").style.display="block";
+    })
+    
+    document.querySelector(".close3").onclick = function() {
+        document.getElementById("sbox2").style.display = "none";
+      }
+      
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == sbox3) {
+            document.getElementById("sbox3").style.display = "none";
+        }
+    }
+    
+}
+function sub(){
+    let title = document.getElementById("title").value;
+    let content=document.getElementById("text-area").value;
+    let obj={
+        title: title,
+        content: content
+    }
+    let arr = JSON.parse(localStorage.getItem("arr")) || [];
+    arr.push(obj)
+    localStorage.setItem("arr",JSON.stringify(arr))
+    
+
+} 
+
